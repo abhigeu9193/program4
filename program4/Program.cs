39197628c1070ea9f601900fc678e28d45de9910
Abhishek <abhigeu9193@gmail.com>
2022-04-26 16:49:09 +0530

    Initial Commit

---
+++ program4/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+class program4
+{
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_NUMBER = 2;
+    public const int EMP_RATE_PER_HOUR = 60;
+    static void Main(string[] args)
+    {
+        int empHrs = 0;
+        int empWage = 0;
+        Random random = new Random();
+        int empCheck = random.Next(0, 2);
+
+        switch (empCheck)
+        {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_NUMBER:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+
+                break;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Emp Wage :" + empWage);
+        Console.ReadKey();
+
+
+    }
+}
